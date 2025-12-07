0edfbfce8517b05f6a99688b22b502966d698412
duynguyen1311 <duy0256@gmail.com>
2025-12-07 11:19:39 +0700

    update

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 provider "aws" {
   region = var.region
 }
-
+locals {
+  tags: {
+    Name: "myEC2"
+  }
+}
 resource "aws_instance" "ansible_server" {
   ami           = "ami-047126e50991d067b"
   instance_type = "t3.small"
@@ -9,4 +13,5 @@ resource "aws_instance" "ansible_server" {
   lifecycle {
     create_before_destroy = true
   }
+  tags = local.tags
 }
